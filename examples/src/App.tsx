e0396fd27c7ef962b34a082159689de8b58635e7
saurabh <saurav.gajjar@constems-ai.com>
2024-04-21 22:03:33 +0530

    updated readme

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,34 +1,83 @@
-import { useRef } from 'react'
+import React, { useRef } from "react";
 import { DynamicDiv } from '../../dist/esm/index'
-import './App.css'
+// import { DynamicDiv } from "dynamic-div";
+import { Radio } from "antd";
 
-function App() {
-  const dragRef = useRef(null)
+const options = [
+  {
+    label: "No target specified",
+    value: "no-target",
+  },
+  {
+    label: "Custom DOM target",
+    value: "custom-target",
+  },
+];
+
+export default function App() {
+  const dragRef = useRef(null);
+  const [selectedTarget, setSelectedTarget] = React.useState("no-target");
+
+  const onChange = ({ target: { value } }: {target: any}) => {
+    setSelectedTarget(value);
+  };
 
   return (
     <div>
-      <DynamicDiv scalable shadow top={20} left={50} draggableRef={dragRef}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '15em',
-            padding: '1em',
-            borderRadius: '10px',
-            boxShadow: '0px 5px 20px -5px rgba(0, 0, 0, 0.25)',
-          }}
-        >
-          <h3 ref={dragRef}>drag this !!!</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusantium aliquam recusandae alias nihil
-            eius est quis quas at, vitae, odit dolor deleniti quaerat. Laboriosam ut repellendus expedita eaque aliquid.
-          </p>
-        </div>
-      </DynamicDiv>
+      <Radio.Group
+        options={options}
+        onChange={onChange}
+        value={selectedTarget}
+        optionType="button"
+      />
+      {selectedTarget === "no-target" && (
+        <DynamicDiv scalable shadow top={100} left={50}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "15em",
+              padding: "1em",
+              borderRadius: "10px",
+              boxShadow: "0px 5px 20px -5px rgba(0, 0, 0, 0.25)",
+            }}
+          >
+            <h3>drag this !!!</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+              accusantium aliquam recusandae alias nihil eius est quis quas at,
+              vitae, odit dolor deleniti quaerat. Laboriosam ut repellendus
+              expedita eaque aliquid.
+            </p>
+          </div>
+        </DynamicDiv>
+      )}
+      {selectedTarget === "custom-target" && (
+        <DynamicDiv scalable shadow top={100} left={50} draggableRef={dragRef}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "15em",
+              padding: "1em",
+              borderRadius: "10px",
+              boxShadow: "0px 5px 20px -5px rgba(0, 0, 0, 0.25)",
+            }}
+          >
+            <h3 ref={dragRef}>drag this !!!</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+              accusantium aliquam recusandae alias nihil eius est quis quas at,
+              vitae, odit dolor deleniti quaerat. Laboriosam ut repellendus
+              expedita eaque aliquid.
+            </p>
+          </div>
+        </DynamicDiv>
+      )}
     </div>
-  )
+  );
 }
-
-export default App
